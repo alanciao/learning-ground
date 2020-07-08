c829a64481fc9acf55d280ea7ffa9bc61588ee8c
qiaotianyuan <qiaotianyuan@codemao.cn>
2020-07-08 12:40:48 +0800

    @initial:build project

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+export function main() {
+  ReactDOM.render(
+    <div>Hello</div>,
+    document.getElementById('root'),
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'build/'),
+    filename: '[name].bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['css-loader', 'style-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+  ],
+};
